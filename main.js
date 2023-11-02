2da5b6f7c289f10af306ba408aee8e23836994b0
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-11-02 15:40:50 +0100

    salvataggio

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 let immagineCorrente = 0;
-const immagini = document.querySelectorAll("#contianer img");
+const immagini = document.querySelectorAll("#container img");
 immagini[immagineCorrente].classList.add("active")
+
+
+document.getElementById("avanti").addEventListener("click", function(){
+
+ if (immagineCorrente < immagini.length -1 ){
+    
+    immagini[immagineCorrente].classList.remove("active")
+    immagineCorrente++;
+    immagini[immagineCorrente].classList.add("active")
+ }
+
+});
+
+document.getElementById("indietro").addEventListener("click", function(){
+
+    if (immagineCorrente > 0){
+
+    immagini[immagineCorrente].classList.remove("active")
+    immagineCorrente--;
+    immagini[immagineCorrente].classList.add("active")
+
+}
+});
+
+
